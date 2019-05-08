e86850aa4c43a6b1e6a58b4c6c540fafff45f8de
mtan96 <muammer.tan@ug.bilkent.edu.tr>
2019-05-08 21:11:37 +0300

    new prints

--- alm_project/src/alm_project/HelloWorld.java
+++ alm_project/src/alm_project/HelloWorld.java
@@ -5,6 +5,13 @@ public class HelloWorld {
 	public static void main(String [] args)
 	{
 		System.out.println("sa");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");	
+		
 	}
-
+	
 }
